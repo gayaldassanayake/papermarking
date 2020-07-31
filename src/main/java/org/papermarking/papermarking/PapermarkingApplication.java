566d8808afce348d3a700e50ea474bb5e9d25305
gayal <g.c.dassanayake@gmail.com>
2020-07-31 15:19:42 +0530

    Initial commit

---
+++ src/main/java/org/papermarking/papermarking/PapermarkingApplication.java
@@ -0,0 +1,13 @@
+package org.papermarking.papermarking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapermarkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PapermarkingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/papermarking/papermarking/PapermarkingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.papermarking.papermarking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapermarkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
