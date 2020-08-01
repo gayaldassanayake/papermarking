9b0b32219c55acda4884bea3ed9f77cf2913076f
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2020-08-02 00:38:44 +0530

    rename authentication document

--- src/main/java/org/papermarking/papermarking/model/Authentication.java
+++ src/main/java/org/papermarking/papermarking/model/Authentication.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "students")
+@Document(collection = "authentication")
 public class Authentication {
     @Id
     private String id;
