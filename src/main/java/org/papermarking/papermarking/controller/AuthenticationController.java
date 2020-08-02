02b6d47de927ec4a3b56fe4d88aed19d4ca27be4
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2020-08-03 00:01:02 +0530

    Update AuthenticationController.java

--- src/main/java/org/papermarking/papermarking/controller/AuthenticationController.java
+++ src/main/java/org/papermarking/papermarking/controller/AuthenticationController.java
@@ -2,7 +2,7 @@ package org.papermarking.papermarking.controller;
 
 import java.util.List;
 
-import org.papermarking.papermarking.model.Authentication;
+import org.papermarking.papermarking.model.userModel.Authentication;
 import org.papermarking.papermarking.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
